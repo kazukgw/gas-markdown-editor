18ede56d888032ee864fe5854269243fdc679d79
kazukgw <kazukgw@gmail.com>
2021-12-04 04:04:00 +0300

    Update

--- src/_js_main.jsx
+++ src/_js_main.jsx
@@ -3,17 +3,16 @@ import * as ReactDOM from "react-dom";
 
 import { Transformer } from "markmap-lib";
 
-const transformer = new Transformer()
+const transformer = new Transformer();
 
 const { markmap } = window;
-const {Markmap, loadCSS, loadJS} = markmap;
+const { Markmap, loadCSS, loadJS } = markmap;
 
 const GS = google.script;
 const CONFIG = JSON.parse(document.getElementById("config").textContent);
 const FILE_ID = CONFIG["fileId"];
 const FILE_NAME = CONFIG["fileName"];
 const FILE_URL = CONFIG["fileUrl"];
-
 
 class Editor extends React.Component {
   constructor(props) {
@@ -26,26 +25,26 @@ class Editor extends React.Component {
     const self = this;
     const codeMirror = CodeMirror.fromTextArea(
       document.getElementById("editor"),
-        {
-          lineNumbers: true,
-          mode: "text/x-markdown",
-          keyMap: "vim",
-          matchBrackets: true,
-          showCursorWhenSelecting: true,
-          theme: "monokai",
-          showTrailingSpace: true,
-          styleActiveLine: true,
-          foldGutter: true,
-          gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
-        }
-      );
+      {
+        lineNumbers: true,
+        mode: "text/x-markdown",
+        keyMap: "vim",
+        matchBrackets: true,
+        showCursorWhenSelecting: true,
+        theme: "monokai",
+        showTrailingSpace: true,
+        styleActiveLine: true,
+        foldGutter: true,
+        gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+      }
+    );
     codeMirror.on("change", this.handleCodeMirrorChange.bind(this));
     this.setState({ codeMirror: codeMirror });
 
     GS.run
-      .withSuccessHandler((ret)=>{
+      .withSuccessHandler((ret) => {
         console.log("read");
-                codeMirror.setOption("extraKeys", {
+        codeMirror.setOption("extraKeys", {
           Tab: function (cm) {
             var spaces = Array(cm.getOption("indentUnit") + 1).join(" ");
             cm.replaceSelection(spaces);
@@ -53,7 +52,7 @@ class Editor extends React.Component {
         });
         codeMirror.setValue(ret["content"]);
       })
-      .withFailureHandler((ret)=>{
+      .withFailureHandler((ret) => {
         console.log(ret);
         alert(ret);
       })
@@ -74,36 +73,36 @@ class Editor extends React.Component {
   }
 }
 
-const renderMarkmap = (function() {
+const renderMarkmap = (function () {
   var interval = 1000;
   var time = Date.now(),
-      lag,
-      debounceTimer,
-      debounceDelay = 16;
+    lag,
+    debounceTimer,
+    debounceDelay = 16;
 
-  return function(markmap, content) {
+  return function (markmap, content) {
     lag = time + interval - Date.now();
     if (lag < 0) {
       //console.log( time + "：throttle：" + lag);
-      const {root} = transformer.transform(content || "")
+      const { root } = transformer.transform(content || "");
       markmap.setData(root);
       time = Date.now();
     } else {
       clearTimeout(debounceTimer);
-      debounceTimer = setTimeout(function() {
+      debounceTimer = setTimeout(function () {
         //console.log( time + "：debounce：" + (interval - lag + debounceDelay));
-        const {root} = transformer.transform(content || "")
+        const { root } = transformer.transform(content || "");
         markmap.setData(root);
-      }, (interval - lag + debounceDelay));
+      }, interval - lag + debounceDelay);
     }
-  }
+  };
 })();
 
 class Preview extends React.Component {
-  constructor(props)  {
+  constructor(props) {
     super(props);
 
-    this.state = {mode: "markmap", markmap: null};
+    this.state = { mode: "markmap", markmap: null };
   }
 
   previewHtml() {
@@ -111,13 +110,13 @@ class Preview extends React.Component {
   }
 
   componentDidMount() {
-    const {root, features} = transformer.transform(this.props.content || "")
-    const {styles, scripts} = transformer.getUsedAssets(features);
+    const { root, features } = transformer.transform(this.props.content || "");
+    const { styles, scripts } = transformer.getUsedAssets(features);
 
     if (styles) loadCSS(styles);
     if (scripts) loadJS(scripts, { getMarkmap: () => markmap });
-    const mm = Markmap.create('#markmap', null, root);
-    this.setState({markmap: mm});
+    const mm = Markmap.create("#markmap", null, root);
+    this.setState({ markmap: mm });
   }
 
   componentDidUpdate() {
@@ -125,22 +124,22 @@ class Preview extends React.Component {
   }
 
   render() {
-    if(this.state.mode === "markmap") {
-      return (<svg id="markmap"/>)
+    if (this.state.mode === "markmap") {
+      return <svg id="markmap" />;
     } else {
-      return (<div dangerouslySetInnerHTML={this.previewHtml.bind(this)()} />);
+      return <div dangerouslySetInnerHTML={this.previewHtml.bind(this)()} />;
     }
   }
 }
 
-const saveAsMarkdownFile = (function() {
-  var interval = 5000;
+const saveAsMarkdownFile = (function () {
+  var interval = 7000;
   var time = Date.now(),
-      lag,
-      debounceTimer,
-      debounceDelay = 16;
+    lag,
+    debounceTimer,
+    debounceDelay = 16;
 
-  return function(content, callback) {
+  return function (content, callback) {
     lag = time + interval - Date.now();
     if (lag < 0) {
       //console.log( time + "：throttle：" + lag);
@@ -157,22 +156,23 @@ const saveAsMarkdownFile = (function() {
       time = Date.now();
     } else {
       clearTimeout(debounceTimer);
-      debounceTimer = setTimeout(function() {
+      debounceTimer = setTimeout(function () {
         //console.log( time + "：debounce：" + (interval - lag + debounceDelay));
         GS.run
-        .withSuccessHandler((_) => {
-          console.log("save: " + FILE_ID);
-          callback();
-        })
-        .withFailureHandler((ret) => {
-          console.log(ret);
-          alert(ret);
-        })
-        .saveAsMarkdownFile(FILE_ID, content);
-      }, (interval - lag + debounceDelay));
+          .withSuccessHandler((_) => {
+            console.log("save: " + FILE_ID);
+            callback();
+          })
+          .withFailureHandler((ret) => {
+            console.log(ret);
+            alert(ret);
+          })
+          .saveAsMarkdownFile(FILE_ID, content);
+      }, interval - lag + debounceDelay);
     }
-  }
+  };
 })();
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -191,8 +191,8 @@ class App extends React.Component {
 
   saveAsMarkdownFile(content) {
     const self = this;
-    saveAsMarkdownFile(content, ()=>{
-      self.setState({savedAt: new Date()})
+    saveAsMarkdownFile(content, () => {
+      self.setState({ savedAt: new Date() });
     });
   }
 
@@ -201,7 +201,9 @@ class App extends React.Component {
       <div id="app-container">
         <div id="header">
           <span class="title">{this.state.title}</span>
-          <span class="saved-at">saved at: {this.state.savedAt.toLocaleString()}</span>
+          <span class="saved-at">
+            saved at: {this.state.savedAt.toLocaleString()}
+          </span>
         </div>
         <Editor onChangeContent={this.changeContent.bind(this)} />
         <Preview content={this.state.content} />
