caf61d28d2108a2e6d160c2bfa2d67b68f5ad523
kazukgw <kazukgw@gmail.com>
2022-06-25 22:42:24 +0900

    update

--- editor/src/lib/components/ViewerMarked.svelte
+++ editor/src/lib/components/ViewerMarked.svelte
@@ -1,13 +1,99 @@
 <script lang="ts">
-
   export let content;
   export let mode;
 
-  let innerHtml = "";
-  content.subscribe((value)=>{
-    innerHtml = marked.parse(value || "");
-  });
+  const jsYaml = window["jsyaml"];
+  if(jsYaml == null) {
+    throw new Error("jsYaml is undefined");
+  }
 
+  let innerHtml = "";
+  let frontMatter = {};
+
+  const fmparse = (value)=>{
+    let i = 0;
+    let inFm = false;
+    let lineString = "";
+    let lineCnt = 0;
+    let fmLinePatern = new RegExp(/^---\s*$/);
+    let blankLinePatern = new RegExp(/^\s*$/);
+    let fmContent = "";
+    let results = {
+      fmContent: "",
+      body:""
+    };
+    for(; i < value.length; i++) {
+      const s = value[i];
+      if(s === "\n") {
+        if(inFm) {
+          if(fmLinePatern.test(lineString)) {
+            results.body = value.slice(i);
+            return results;
+          } else {
+            results.fmContent += lineString + "\n";
+          }
+        } else {
+          if(fmLinePatern.test(lineString)) {
+            inFm = true;
+          } else if(!blankLinePatern.test(lineString)) {
+            results.body = value;
+            return results;
+          }
+        }
+        lineString = "";
+      } else {
+        lineString += s;
+      }
+      lineCnt++;
+      if(lineCnt > 100) {
+        results.body = value;
+        return results;
+      }
+    }
+    results.body = value;
+    return results;
+  };
+
+
+  const parse = (function () {
+    var interval = 600;
+    var time = Date.now(),
+      lag,
+      debounceTimer,
+      debounceDelay = 16;
+
+    return function (content) {
+      lag = time + interval - Date.now();
+      if (lag < 0) {
+        time = Date.now();
+        const results = fmparse(content);
+        try {
+          frontMatter = jsyaml.load(results.fmContent);
+        } catch(e) {
+          frontMatter = {error: e};
+        }
+        console.log(frontMatter);
+        innerHtml = marked.parse(results.body);
+      } else {
+        clearTimeout(debounceTimer);
+        debounceTimer = setTimeout(function () {
+          const results = fmparse(content);
+          try {
+            frontMatter = jsyaml.load(results.fmContent);
+          } catch(e) {
+            frontMatter = {error: e};
+          }
+          console.log(frontMatter);
+          innerHtml = marked.parse(results.body);
+        }, interval - lag + debounceDelay);
+      }
+    };
+  })();
+
+
+  content.subscribe((value)=>{
+    parse(value || "");
+  });
 </script>
 
 <div class="marked-wrapper">
@@ -17,8 +103,6 @@
 </div>
 
 <style lang="scss">
-@import url(https://fonts.googleapis.com/css?family=Droid+Sans:300|Roboto+Slab:300);
-
 .marked-wrapper {
   width: 100%;
   height: 100%;
